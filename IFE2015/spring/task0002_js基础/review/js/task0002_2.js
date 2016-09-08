ed16c18af72b28d609782c500ce08b60a43337fd
ctrlf <lfy3715@gmail.com>
2016-09-09 01:11:27 +0800

    no message

--- IFE2015/spring/task0002_js基础/review/js/task0002_2.js
+++ IFE2015/spring/task0002_js基础/review/js/task0002_2.js
@@ -4,6 +4,7 @@
  */
 
 /**
+ * 对目标字符串格式化
  * @param {string} [source] [目标字符串]
  * @param {Object|string} [opts] [提供相应字符串对象或者多个字符串]
  *
@@ -11,4 +12,9 @@
 
 function format(source, opts) {
 	source = String(source);
+	 var date = Array.prototype.slice.call(arguments,
+	 	if (data.length) {
+	 		// statement
+	 	}
+	 	)
 }
