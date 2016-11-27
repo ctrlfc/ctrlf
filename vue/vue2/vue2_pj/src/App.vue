58eb108fe74286bf6cf12da0550c6923cdef50a1
ctrlf <lfy3715@gmail.com>
2016-11-28 01:40:20 +0800

    no message

---
+++ vue/vue2/vue2_pj/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <!-- 一个组件下只能有一个并列的 div，可以这么写，所以复制官网示例的时候只要复制 div 里面的内容就好。 -->
+
+    <div id="app-6">
+        <p>{{message}}</p>
+        <input v-model="message">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                message: 'Vue2.0',
+                msg: 'Hello Vue!'
+            }
+        }
+    }
+</script>
+
