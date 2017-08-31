57db5a61339eb06d4f85f36e6c8a162f509a681f
ctrlf <lfy3715@gmail.com>
2017-09-01 00:07:25 +0800

    枚举

--- js/对象/枚举/enumeration.js
+++ js/对象/枚举/enumeration.js
@@ -2,22 +2,25 @@
 
 // 用hasOwnProperty 方法除去不需要的原型值
 
-// 用typeof 除去 函数
+// 用typeof 除去 可能是函数 的值
+
+let another_stooge = [];
 
 let name;
 
 for (name in another_stooge) {
-    if (typeof another_stooge[name] !== 'function') {
+    if (typeof another_stooge[name] !== "function") {
         document.writeln(name + ":" + another_stooge[name]);
+        console.log(name + ":" + another_stooge[name]);
     }
 }
 
-// 用 for 创建数组确保顺序 
+// 用 for 创建数组确保顺序 ,且不出现原型链中的属性
 
-// let i;
+let i;
 
-// const properties = ["first-name", "middle-name", "last-name", "profession"];
+const properties = ["first-name", "middle-name", "last-name", "profession"];
 
-// for (i = 0; i < properties.length; i += 1) {
-//     document.writeln(properties[i] + ":" + another_stooge[properties[i]]);
-// }
+for (i = 0; i < properties.length; i += 1) {
+    document.writeln(properties[i] + ":" + another_stooge[properties[i]]);
+}
