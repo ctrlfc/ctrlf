3ebb63531088f936d13498db5b0fbba2a98947f1
ctrlf <lfy3715@gmail.com>
2017-09-28 17:39:06 +0800

    sort2 排序

--- js/方法/Array/array.sort.js
+++ js/方法/Array/array.sort.js
@@ -30,11 +30,54 @@ console.log(m);
 
 // 并返回一个可以用来对包含该成员的对象数组进行排序的比较函数
 
-var by = function (name) {
-    return function (o, p) {
+var by = function(name) {
+    return function(o, p) {
         var a, b;
-        
-    }
+        if (typeof o === "object" && typeof p === "object" && o && p) {
+            a = o[name];
+            b = p[name];
+            if (a === b) {
+                return 0;
+            }
+            if (typeof a === typeof b) {
+                return a < b ? -1 : 1;
+            }
+            return typeof a < typeof b ? -1 : 1;
+        } else {
+            throw {
+                name: "Error",
+                message: "Expected an object when sorting by" + name
+            };
+        }
+    };
+};
+
+var s = [
+    { first: "Joe", last: "Besser" },
+    { first: "Moe", last: "Howard" },
+    { first: "Joe", last: "DeRita" },
+    { first: "Joe", last: "DeRita" },
+    { first: "Joe", last: "DeRita" },
+    { first: "Joe", last: "DeRita" }
+];
+
+s.sort(by("first"));
+
+// sort 是不稳定的
+
+s.sort(by("first")).sort(by("last"));
+
+// 如果想要正确的序列
+
+// by函数接受一个成员名字字符串和一个可选的次要比较函数为参数
+
+// 并返回一个可以用来对包含改成员数组进行排序的比较函数
+
+// 当o[name]和p[name]相等,次要比较函数被用来
+
+var by = function (name, minor) {
+    var a ,b
+    return function (o, p) {
+        // if(o &&p&&typeof===)
+    }    
 }
-
-
