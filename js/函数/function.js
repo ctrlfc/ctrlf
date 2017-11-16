37b07abe133cb1863a12625a72ed76567348d23c
ctrlf <lfy3715@gmail.com>
2017-11-16 23:38:48 +0800

    秘密花园 名词解释顺序

--- js/函数/function.js
+++ js/函数/function.js
@@ -534,3 +534,16 @@ var myvar = "myvalue";
     alert(myvar); //undefined
     var myvar = "local value";
 })();
+
+// 名称解析顺序
+
+// JavaScript 中的所有作用域，包括全局作用域，都有一个特别的名称 this 指向当前对象。
+
+// 函数作用域内也有默认的变量 arguments，其中包含了传递到函数中的参数。
+
+// 比如，当访问函数内的 foo 变量时，JavaScript 会按照下面顺序查找：
+
+// 当前作用域内是否有 var foo 的定义。
+// 函数形式参数是否有使用 foo 名称的。
+// 函数自身是否叫做 foo。
+// 回溯到上一级作用域，然后从 #1 重新开始。
