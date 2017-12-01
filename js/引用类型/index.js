db8c3dcf89e67fdf721f7a120917e7c282b76b62
ctrlf <lfy3715@gmail.com>
2017-12-01 19:14:50 +0800

    秘密花园 等于操作符

--- js/引用类型/index.js
+++ js/引用类型/index.js
@@ -4,3 +4,18 @@
 
 // JavaScript 有两种方式判断两个值是否相等。
 
+// 等于操作符
+
+// 等于操作符由两个等号组成:==
+
+// js是弱类型语言,这就意味着,等于操作符会为了比较两个值而进行强制类型转换
+
+""           ==   "0"           // false
+0            ==   ""            // true
+0            ==   "0"           // true
+false        ==   "false"       // false
+false        ==   "0"           // true
+false        ==   undefined     // false
+false        ==   null          // false
+null         ==   undefined     // true
+" \t\r\n"    ==   0             // true
